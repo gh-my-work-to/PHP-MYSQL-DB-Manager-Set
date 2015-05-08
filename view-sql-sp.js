7f2366f87ec531817ff6f6d32d0d43a1e4284d2d
gh-my-work-to <my.work.tokorozawa@gmail.com>
2015-05-08 23:59:22 +0900

    Update view-sql-sp.js

--- view-sql-sp.js
+++ view-sql-sp.js
@@ -1,6 +1,6 @@
 $('#setHeader').click(function()
 {
-	var str = "select {{header}}_posts.ID, {{header}}_posts.post_content, {{header}}_posts.post_date, tmp_last.slg\n \n from {{header}}_posts right join\n\n (select tmp_taxid.tetaxid, tmp_taxid.termid, tmp_taxid.slg as slg,\n\n {{header}}_term_relationships.object_id as oid\n\n from {{header}}_term_relationships right join\n\n (select {{header}}_term_taxonomy.term_taxonomy_id as tetaxid, \n \n {{header}}_term_taxonomy.term_id as termid,\n \n {{header}}_terms.slug as slg\n \n from {{header}}_term_taxonomy\n \n right join {{header}}_terms\n \n on {{header}}_term_taxonomy.term_id = {{header}}_terms.term_id) as tmp_taxid\n\n on {{header}}_term_relationships.term_taxonomy_id = tmp_taxid.tetaxid) as tmp_last\n\n on {{header}}_posts.ID = tmp_last.oid\n\n where tmp_last.slg='{{slug}}'"
+	var str = "SELECT {{header}}_posts.post_title, {{header}}_posts.post_content, {{header}}_posts.post_date, {{header}}_terms.slug\n \n FROM {{header}}_posts \n\n RIGHT JOIN {{header}}_term_relationships ON {{header}}_term_relationships.object_id = {{header}}_posts.ID\n\n RIGHT JOIN {{header}}_term_taxonomy ON {{header}}_term_taxonomy.term_taxonomy_id = {{header}}_term_relationships.term_taxonomy_id\n\n RIGHT JOIN {{header}}_terms ON {{header}}_terms.term_id = {{header}}_term_taxonomy.term_id\n\n WHERE {{header}}_terms.slug = '{{slug}}'\n \n ORDER BY {{header}}_posts.post_date DESC\n \n LIMIT 0, 1"
 	
 	.split('{{header}}').join($('#header').val())
 	.split('{{slug}}').join($('#slug').val());
